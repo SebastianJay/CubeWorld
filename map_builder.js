4bb496d05c7b8a93b61beae7f0e30e46ccadf254
SebastianJay <jgs3cd@virginia.edu>
2015-04-30 02:04:33 -0400

    created all faces of cube

--- map_builder.js
+++ map_builder.js
@@ -1,36 +1,86 @@
 
-var mapBuffer;
-var mapTextureBuffer;
-var mapVertexIndexBuffer;
+var tileNum = 20;
+var cubeSideLength = 40.0;
+
+var mapPositionStack = [];
+var mapTextureStack = [];
+var mapVertexIndexStack = [];
+
+function randInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function createMap()
 {
-	//alert("creating arrays");
+	createFace(-1, 0);
+	createFace(1, 0);
+	createFace(-1, 1);
+	createFace(1, 1);
+	createFace(-1, 2);
+	createFace(1, 2);
+}
 
+function createFace(sign, num)
+{
 	var vertices = [];
 	var textureCoords = [];
 	var mapVertexIndices = [];
-	var tileLength = 20;
-	for (i = 0; i < tileLength; i++) {
-		for (j = 0; j < tileLength; j++) {
-			vertices.push(-20.0 + (i*2.0));
-			vertices.push(0.0);
-			vertices.push(-20.0 + (j*2.0));
-			//vertices.push(0.0);
+	for (i = 0; i < tileNum; i++) {
+		for (j = 0; j < tileNum; j++) {
+			var elevation = randInt(-1, 2);
+			var base = -cubeSideLength / 2;
+			
+			//there's probably a smarter way to do this.
+			if (num == 0) {
+				vertices.push(base + (i*2.0));
+				vertices.push(sign*base + elevation * 0.2);
+				vertices.push(base + (j*2.0));
+				
+				vertices.push(base + ((i+1)*2.0));
+				vertices.push(sign*base + elevation * 0.2);
+				vertices.push(base + (j*2.0));
 
-			vertices.push(-20.0 + ((i+1)*2.0));
-			vertices.push(0.0);
-			vertices.push(-20.0 + (j*2.0));
-			//vertices.push(0.0);
+				vertices.push(base + (i*2.0));
+				vertices.push(sign*base + elevation * 0.2);
+				vertices.push(base + ((j+1)*2.0));
 
-			vertices.push(-20.0 + (i*2.0));
-			vertices.push(0.0);
-			vertices.push(-20.0 + ((j+1)*2.0));
-			//vertices.push(0.0);
+				vertices.push(base + ((i+1)*2.0));
+				vertices.push(sign*base + elevation * 0.2);
+				vertices.push(base + ((j+1)*2.0));
+			}
+			else if (num == 1) {
+				vertices.push(sign*base + elevation * 0.2);
+				vertices.push(base + (i*2.0));
+				vertices.push(base + (j*2.0));
 
-			vertices.push(-20.0 + ((i+1)*2.0));
-			vertices.push(0.0);
-			vertices.push(-20.0 + ((j+1)*2.0));
-	//		vertices.push(0.0);
+				vertices.push(sign*base + elevation * 0.2);				
+				vertices.push(base + ((i+1)*2.0));
+				vertices.push(base + (j*2.0));
+
+				vertices.push(sign*base + elevation * 0.2);
+				vertices.push(base + (i*2.0));
+				vertices.push(base + ((j+1)*2.0));
+
+				vertices.push(sign*base + elevation * 0.2);
+				vertices.push(base + ((i+1)*2.0));
+				vertices.push(base + ((j+1)*2.0));
+			} else if (num == 2) {
+				vertices.push(base + (i*2.0));
+				vertices.push(base + (j*2.0));
+				vertices.push(sign*base + elevation * 0.2);
+
+				vertices.push(base + ((i+1)*2.0));
+				vertices.push(base + (j*2.0));
+				vertices.push(sign*base + elevation * 0.2);				
+
+				vertices.push(base + (i*2.0));
+				vertices.push(base + ((j+1)*2.0));
+				vertices.push(sign*base + elevation * 0.2);				
+
+				vertices.push(base + ((i+1)*2.0));
+				vertices.push(base + ((j+1)*2.0));
+				vertices.push(sign*base + elevation * 0.2);				
+			}
 			
 			textureCoords.push(0.0);
 			textureCoords.push(0.0);
@@ -44,60 +94,37 @@ function createMap()
 			textureCoords.push(1.0);
 			textureCoords.push(1.0);
 			
-			mapVertexIndices.push((i*tileLength + j)*4);
-			mapVertexIndices.push((i*tileLength + j)*4+1);
-			mapVertexIndices.push((i*tileLength + j)*4+3);
-			mapVertexIndices.push((i*tileLength + j)*4);
-			mapVertexIndices.push((i*tileLength + j)*4+3);
-			mapVertexIndices.push((i*tileLength + j)*4+2);
-			
-			/*
-			mapVertexIndices.push((i)*4);
-			mapVertexIndices.push((i)*4+1);
-			mapVertexIndices.push((i)*4+3);
-			mapVertexIndices.push((i)*4);
-			mapVertexIndices.push((i)*4+3);
-			mapVertexIndices.push((i)*4+2);
-			*/
+			mapVertexIndices.push((i*tileNum + j)*4);
+			mapVertexIndices.push((i*tileNum + j)*4+1);
+			mapVertexIndices.push((i*tileNum + j)*4+3);
+			mapVertexIndices.push((i*tileNum + j)*4);
+			mapVertexIndices.push((i*tileNum + j)*4+3);
+			mapVertexIndices.push((i*tileNum + j)*4+2);
 		}
 	}
-	//alert("All done");
 	
-	//var vertices = [
-	//	0.0, 0.0, 1.0,
-	//	1.0, 0.0, 1.0,
-	//	0.0, 1.0, 1.0,
-	//	1.0, 1.0, 1.0,
-	//];
 	
-	mapBuffer = gl.createBuffer();
+	var mapBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, mapBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	mapBuffer.itemSize = 3;
-	mapBuffer.numItems = tileLength * tileLength * 4;
+	mapBuffer.numItems = tileNum * tileNum * 4;
 
-	mapTextureBuffer = gl.createBuffer();
+	var mapTextureBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, mapTextureBuffer);
-	//var textureCoords = [
-	//	0.0, 0.0,
-	//	1.0, 0.0,
-	//	0.0, 1.0,
-	//	1.0, 1.0,
-	//];
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
 	mapTextureBuffer.itemSize = 2;
-	mapTextureBuffer.numItems = tileLength * tileLength * 4;
+	mapTextureBuffer.numItems = tileNum * tileNum * 4;
 	
-	mapVertexIndexBuffer = gl.createBuffer();
+	var mapVertexIndexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mapVertexIndexBuffer);
-	//var mapVertexIndices = [
-	//	0, 1, 3,      0, 3, 2,    // Front face
-	//];
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(mapVertexIndices), gl.STATIC_DRAW);
 	mapVertexIndexBuffer.itemSize = 1;
-	mapVertexIndexBuffer.numItems = tileLength * tileLength * 6;
-
-	//alert("GOT TO END");
+	mapVertexIndexBuffer.numItems = tileNum * tileNum * 6;
+	
+	mapPositionStack.push(mapBuffer);
+	mapTextureStack.push(mapTextureBuffer);
+	mapVertexIndexStack.push(mapVertexIndexBuffer);
 }
 
 function drawMap() 
@@ -107,14 +134,20 @@ function drawMap()
 	mat4.rotate(mvMatrix, degToRad(-yaw), [0, 1, 0]);
 	mat4.translate(mvMatrix, [-xPos, -yPos, -zPos]);
 	
-	gl.bindBuffer(gl.ARRAY_BUFFER, mapBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mapBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.bindBuffer(gl.ARRAY_BUFFER, mapTextureBuffer);
-	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, mapTextureBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, neheTexture);
-	gl.uniform1i(shaderProgram.samplerUniform, 0);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mapVertexIndexBuffer);
-	setMatrixUniforms();
-	gl.drawElements(gl.TRIANGLES, mapVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+	for (i = 0; i < mapPositionStack.length; i++) {
+		var mapBuffer = mapPositionStack[i];
+		var mapTextureBuffer = mapTextureStack[i];
+		var mapVertexIndexBuffer = mapVertexIndexStack[i];
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, mapBuffer);
+		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mapBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		gl.bindBuffer(gl.ARRAY_BUFFER, mapTextureBuffer);
+		gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, mapTextureBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, neheTexture);
+		gl.uniform1i(shaderProgram.samplerUniform, 0);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mapVertexIndexBuffer);
+		setMatrixUniforms();
+		gl.drawElements(gl.TRIANGLES, mapVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+	}
 }
