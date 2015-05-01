040ea2af825165a8bf851805b53f8e2e356c23a4
SebastianJay <jgs3cd@virginia.edu>
2015-05-01 16:42:42 -0400

    created sidings

--- map_builder.js
+++ map_builder.js
@@ -8,18 +8,30 @@ var mapTextureStack = [];
 var mapVertexIndexStack = [];
 var elevationStack = [];
 
+var sidingPositionStack = [];
+var sidingTextureStack = [];
+var sidingVertexIndexStack = [];
+
 function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function createMap()
 {
-	createFace(-1, 0);
-	createFace(1, 0);
-	createFace(-1, 1);
-	createFace(1, 1);
-	createFace(-1, 2);
-	createFace(1, 2);
+	//order must be consistent here
+	createFace(-1, 0);		//grass
+	createFace(1, 1);		//shadow
+	createFace(-1, 2);		//metal
+	createFace(1, 3);		//earth
+	createFace(-1, 4);		//fire
+	createFace(1, 5);		//water
+	
+	createSiding(-1, 0);
+	createSiding(1, 1);
+	createSiding(-1, 2);
+	createSiding(1, 3);
+	createSiding(-1, 4);
+	createSiding(1, 5);
 }
 
 function createFace(sign, num)
@@ -39,58 +51,35 @@ function createFace(sign, num)
 			var elevation = -sign*base + elevationNum * 0.2;
 			elevations.push(elevation);
 			
-			//there's probably a smarter way to do this.
-			if (num == 0) {
-				vertices.push(base + (i*tileLength));
-				vertices.push(elevation);
-				vertices.push(base + (j*tileLength));
-				
-				vertices.push(base + ((i+1)*tileLength));
-				vertices.push(elevation);
-				vertices.push(base + (j*tileLength));
-
-				vertices.push(base + (i*tileLength));
-				vertices.push(elevation);
-				vertices.push(base + ((j+1)*tileLength));
-
-				vertices.push(base + ((i+1)*tileLength));
-				vertices.push(elevation);
-				vertices.push(base + ((j+1)*tileLength));
+			var v1 = [], v2 = [], v3 = [], v4 = [];
+			v1.push(base + i*tileLength);
+			v1.push(base + j*tileLength);
+			v2.push(base + (i+1)*tileLength);
+			v2.push(base + j*tileLength);
+			v3.push(base + i*tileLength);
+			v3.push(base + (j+1)*tileLength);
+			v4.push(base + (i+1)*tileLength);
+			v4.push(base + (j+1)*tileLength);
+			if (num == 0 || num == 1) {
+				v1.splice(1, 0, elevation);
+				v2.splice(1, 0, elevation);
+				v3.splice(1, 0, elevation);
+				v4.splice(1, 0, elevation);
+			} else if (num == 2 || num == 3) {
+				v1.unshift(elevation);
+				v2.unshift(elevation);
+				v3.unshift(elevation);
+				v4.unshift(elevation);
+			} else if (num == 4 || num == 5) {
+				v1.push(elevation);
+				v2.push(elevation);
+				v3.push(elevation);
+				v4.push(elevation);
 			}
-			else if (num == 1) {
-				vertices.push(elevation);
-				vertices.push(base + (i*tileLength));
-				vertices.push(base + (j*tileLength));
+			var allverts = v1.concat(v2).concat(v3).concat(v4);
+			for (var k = 0; k < allverts.length; k++)
+				vertices.push(allverts[k]);
 
-				vertices.push(elevation);				
-				vertices.push(base + ((i+1)*tileLength));
-				vertices.push(base + (j*tileLength));
-
-				vertices.push(elevation);
-				vertices.push(base + (i*tileLength));
-				vertices.push(base + ((j+1)*tileLength));
-
-				vertices.push(elevation);
-				vertices.push(base + ((i+1)*tileLength));
-				vertices.push(base + ((j+1)*tileLength));
-			} else if (num == 2) {
-				vertices.push(base + (i*tileLength));
-				vertices.push(base + (j*tileLength));
-				vertices.push(elevation);
-
-				vertices.push(base + ((i+1)*tileLength));
-				vertices.push(base + (j*tileLength));
-				vertices.push(elevation);				
-
-				vertices.push(base + (i*tileLength));
-				vertices.push(base + ((j+1)*tileLength));
-				vertices.push(elevation);				
-
-				vertices.push(base + ((i+1)*tileLength));
-				vertices.push(base + ((j+1)*tileLength));
-				vertices.push(elevation);				
-			}
-			
 			textureCoords.push(0.0);
 			textureCoords.push(0.0);
 			
@@ -111,7 +100,6 @@ function createFace(sign, num)
 			mapVertexIndices.push((i*tileNum + j)*4+2);
 		}
 	}
-	
 	
 	var mapBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, mapBuffer);
@@ -137,20 +125,144 @@ function createFace(sign, num)
 	elevationStack.push(elevations);
 }
 
-function mapIntToGroundTexture(i)
-{
-	if (i==0)
-		return gl.TEXTURE5;
-	if (i==1)
-		return gl.TEXTURE4;
-	if (i==2)
-		return gl.TEXTURE3;
-	if (i==3)
-		return gl.TEXTURE2;
-	if (i==4)
-		return gl.TEXTURE1;
-	if (i==5)
-		return gl.TEXTURE0;
+//lots of args... w/e
+function createSidingHelper(v1, v2, v3, v4, ind, sign, counter, groundElevation, vertices, textureCoords, vertexIndices) {
+	if (ind == 0 || ind == 1) {
+		v1.splice(1, 0, groundElevation);
+		v2.splice(1, 0, groundElevation);
+		v3.splice(1, 0, groundElevation + sign*tileLength);
+		v4.splice(1, 0, groundElevation + sign*tileLength);
+	} else if (ind == 2 || ind == 3) {
+		v1.unshift(groundElevation);
+		v2.unshift(groundElevation);
+		v3.unshift(groundElevation + sign*tileLength);
+		v4.unshift(groundElevation + sign*tileLength);	
+	} else if (ind == 4 || ind == 5) {
+		v1.push(groundElevation);
+		v2.push(groundElevation);
+		v3.push(groundElevation + sign*tileLength);
+		v4.push(groundElevation + sign*tileLength);	
+	}
+	var allverts = v1.concat(v2).concat(v3).concat(v4);
+	for (var k = 0; k < allverts.length; k++)
+		vertices.push(allverts[k]);
+	
+	textureCoords.push(0.0);
+	textureCoords.push(0.0);
+	
+	textureCoords.push(1.0);
+	textureCoords.push(0.0);
+
+	textureCoords.push(1.0);
+	textureCoords.push(1.0);
+
+	textureCoords.push(0.0);
+	textureCoords.push(1.0);
+
+	vertexIndices.push(counter*4);
+	vertexIndices.push(counter*4+1);
+	vertexIndices.push(counter*4+2);
+	vertexIndices.push(counter*4);
+	vertexIndices.push(counter*4+2);
+	vertexIndices.push(counter*4+3);
+}
+
+function createSiding(sign, ind) {
+	var groundElevations = elevationStack[ind];
+	var vertices = [];
+	var textureCoords = [];
+	var vertexIndices = [];
+	var counter = 0;
+	var base = -cubeSideLength / 2;
+	for (var i = 0; i < tileNum; i++) {
+		for (var j = 0; j < tileNum; j++) {
+			if (i > 0 && -sign*(groundElevations[i*tileNum+j] - groundElevations[(i-1)*tileNum+j]) > 0) {
+				var v1 = [], v2 = [], v3 = [], v4 = [];
+				v1.push(base + i*tileLength);
+				v1.push(base + j*tileLength);
+				v2.push(base + i*tileLength);
+				v2.push(base + (j+1)*tileLength);
+				v3.push(base + i*tileLength);
+				v3.push(base + (j+1)*tileLength);
+				v4.push(base + i*tileLength);
+				v4.push(base + j*tileLength);
+								
+				createSidingHelper(v1,v2,v3,v4, ind, sign, counter, groundElevations[i*tileNum + j],
+						vertices, textureCoords, vertexIndices);
+				counter += 1;
+			}
+			if (i < tileNum-1 && -sign*(groundElevations[i*tileNum+j] - groundElevations[(i+1)*tileNum+j]) > 0) {
+				var v1 = [], v2 = [], v3 = [], v4 = [];
+				v1.push(base + (i+1)*tileLength);
+				v1.push(base + j*tileLength);
+				v2.push(base + (i+1)*tileLength);
+				v2.push(base + (j+1)*tileLength);
+				v3.push(base + (i+1)*tileLength);
+				v3.push(base + (j+1)*tileLength);
+				v4.push(base + (i+1)*tileLength);
+				v4.push(base + j*tileLength);
+				
+				createSidingHelper(v1,v2,v3,v4, ind, sign, counter, groundElevations[i*tileNum + j],
+						vertices, textureCoords, vertexIndices);
+				counter += 1;
+			}
+			if (j > 0 && -sign*(groundElevations[i*tileNum+j] - groundElevations[i*tileNum+j-1]) > 0) {
+				var v1 = [], v2 = [], v3 = [], v4 = [];
+				v1.push(base + i*tileLength);
+				v1.push(base + j*tileLength);
+				v2.push(base + (i+1)*tileLength);
+				v2.push(base + j*tileLength);
+				v3.push(base + (i+1)*tileLength);
+				v3.push(base + j*tileLength);
+				v4.push(base + i*tileLength);
+				v4.push(base + j*tileLength);
+
+				createSidingHelper(v1,v2,v3,v4, ind, sign, counter, groundElevations[i*tileNum + j],
+						vertices, textureCoords, vertexIndices);
+				counter += 1;
+			}
+			if (j < tileNum-1 && -sign*(groundElevations[i*tileNum+j] - groundElevations[i*tileNum+j+1]) > 0) {
+				var v1 = [], v2 = [], v3 = [], v4 = [];
+				v1.push(base + i*tileLength);
+				v1.push(base + (j+1)*tileLength);
+				v2.push(base + (i+1)*tileLength);
+				v2.push(base + (j+1)*tileLength);
+				v3.push(base + (i+1)*tileLength);
+				v3.push(base + (j+1)*tileLength);
+				v4.push(base + i*tileLength);
+				v4.push(base + (j+1)*tileLength);
+
+				createSidingHelper(v1,v2,v3,v4, ind, sign, counter, groundElevations[i*tileNum + j],
+						vertices, textureCoords, vertexIndices);
+				counter += 1;
+			}
+		}
+	}
+	//console.log(counter);
+	//console.log(groundElevations.length);
+	//console.log(elevationStack.length);
+	
+	var sidingBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, sidingBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	sidingBuffer.itemSize = 3;
+	sidingBuffer.numItems = counter * 4;
+
+	var sidingTextureBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, sidingTextureBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+	sidingTextureBuffer.itemSize = 2;
+	sidingTextureBuffer.numItems = counter * 4;
+	
+	var sidingVertexIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sidingVertexIndexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexIndices), gl.STATIC_DRAW);
+	sidingVertexIndexBuffer.itemSize = 1;
+	sidingVertexIndexBuffer.numItems = counter * 6;
+	
+	sidingPositionStack.push(sidingBuffer);
+	sidingTextureStack.push(sidingTextureBuffer);
+	sidingVertexIndexStack.push(sidingVertexIndexBuffer);
 }
 
 function drawMap() 
@@ -201,5 +313,22 @@ function drawMap()
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mapVertexIndexBuffer);
 		setMatrixUniforms();
 		gl.drawElements(gl.TRIANGLES, mapVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+	}
+	
+	for (var i = 0; i < sidingPositionStack.length; i++) {
+		var sidingBuffer = sidingPositionStack[i];
+		var sidingTextureBuffer = sidingTextureStack[i];
+		var sidingVertexIndexBuffer = sidingVertexIndexStack[i];
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, sidingBuffer);
+		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, sidingBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		gl.bindBuffer(gl.ARRAY_BUFFER, sidingTextureBuffer);
+		gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, sidingTextureBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, groundTextureStack[i]);
+		gl.uniform1i(shaderProgram.samplerUniform, 0);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sidingVertexIndexBuffer);
+		setMatrixUniforms();
+		gl.drawElements(gl.TRIANGLES, sidingVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 	}
 }
